38fed1239cd4c15b0d2fbb8bc3dea56ec5d72135
Anunaya07 <coderxxyay@gmail.comgit>
2022-02-12 15:21:44 +0530

    done with auth

--- app.py
+++ app.py
@@ -4,12 +4,8 @@
 from base64 import decode
 from tkinter.messagebox import NO
 from flask import Flask, render_template, url_for, redirect, request,Request, Response, session
-from flask_login import login_user, LoginManager, login_required,logout_user, current_user, UserMixin
 from flask_bcrypt import Bcrypt
-# from database import db, db_init
-# from tables import User, StatusUpload, activityStack
 from forms import LoginForm, RegisterForm
-# from werkzeug.utils import secure_filename
 from flask_session import Session
 from flask_redis import FlaskRedis
 # =============================================================================
@@ -30,24 +26,6 @@ sess.init_app(app)
 # =============================================================================
 
 
-# =============================================================================
-# initalising login Manager
-# =============================================================================
-# loginManager = LoginManager()
-# loginManager.init_app(app)
-# loginManager.login_view = "login"
-
-# @loginManager.user_loader
-# def load_user(user_id):
-#     print("user:", user_id)
-#     return redis_client.smembers(f"user:{int(user_id)}")
-# =============================================================================
-
-# class User(UserMixin):
-#   def is_active(self):
-#      return True
-#   def get_id(self):
-#       return super().get_id()
 
 
 
@@ -76,24 +54,16 @@ def login():
                 user = entities
                 if user:
                     # print(type(form.password.data))
-                    print(user[1])
-                    print(type(user[1]))
+                    # print(user[1])
+                    # print(type(user[1]))
                     if user[1].decode('UTF-8') == username:
                         i=0
                     else:
                         i=1
                     if bcrypt.check_password_hash(user[i], form.password.data):
-                              print("hello")
-                            #   login_user(User) 
                               session['value'] = userID
                               session[userID]=True
                               return redirect(url_for('dashboard'))
-                    
-        # user = User.query.filter_by(username=form.username.data).first()
-    #     if user:
-    #         if bcrypt.check_password_hash(user.password, form.password.data ):
-    #             login_user(user) 
-    #             return redirect(url_for('dashboard'))
     return render_template('login.html', form=form)
 
 # =============================================================================
@@ -103,23 +73,26 @@ def login():
 def signUp():
     form = RegisterForm()
     if form.validate_on_submit():
+        username = form.username.data
+        if redis_client.get(username)!=None:
+            return redirect(url_for('signUp'))
         if (redis_client.get('total_user')==None):
             redis_client.set('total_user', 1)
         else:
             redis_client.incr('total_user')
         userId = redis_client.get('total_user')
         userId = userId.decode('UTF-8')
-        print(userId)
+        # print(userId)
         hashedPassword = bcrypt.generate_password_hash(form.password.data).decode("UTF-8")
         username = form.username.data
         redis_client.set(username, f"user:{userId}")
         redis_client.sadd(f"user:{userId}", username)
         redis_client.sadd(f"user:{userId}", hashedPassword)
-        print(redis_client.smembers(f"user:{userId}"))
-        slist = list(redis_client.smembers(f"user:{userId}"))
-        slist[1] = slist[1].decode('UTF-8')
-        slist[0] = slist[0].decode('UTF-8')
-        print(slist)
+        # print(redis_client.smembers(f"user:{userId}"))
+        # slist = list(redis_client.smembers(f"user:{userId}"))
+        # slist[1] = slist[1].decode('UTF-8')
+        # slist[0] = slist[0].decode('UTF-8')
+        # print(slist)
         return redirect(url_for('login'))
     return render_template('signUp.html', form=form)
 
@@ -143,7 +116,7 @@ def logout():
 def dashboard():
     if session.get(session.get('value'))==True:
         # session[session.get('value')]=False
-        return "welcome to dashboard"
+        return render_template('dashboard.html')
     return redirect(url_for('login'))
 
 
